d0d3bac46a66f8d81c2b5ea9b3362935b8952e0b
josejooj <ribamarjunior@gmail.com>
2022-12-12 00:21:45 -0300

    Agora a URI da Database Mongo funciona por meio de variáveis de ambiente (.env)

--- api/Database.ts
+++ api/Database.ts
@@ -1,7 +1,10 @@
 import { connect, Schema, model, set } from 'mongoose';
 import { URLSchema, URLType } from './Schema';
-import { mongoURL } from '../config.json';
+import { config as ConfigENVFile } from 'dotenv'
 
+ConfigENVFile()
+
+const MongoURI = process.env.MONGOURI as string
 const URLs = model<URLType>('urls', new Schema(URLSchema));
 
 console.log("Tentando entrar no banco de dados...");
@@ -9,7 +12,7 @@ console.log("Tentando entrar no banco de dados...");
 (async () => {
     try {
         set('strictQuery', true)
-        await connect(mongoURL)
+        await connect(MongoURI)
         console.log("Conectado com sucesso ao banco de dados")
     } catch(e) {
         console.log("Failed to connect to database")
